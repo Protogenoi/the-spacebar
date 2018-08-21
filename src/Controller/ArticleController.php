5949042c3387ec4d2842ec53f9293e7988a8a1e5
Michael <michael@adl-crm.uk>
2018-08-21 11:35:19 +0100

    Commented out routes.yawml. Installed annotations and added annotations route to ArticleController.php

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,11 +8,16 @@
 
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ArticleController
 {
+
+    /**
+     * @Route("/")
+     */
+
     public function homepage() {
 
         return new Response("My first page");
