d2a54189f63f9d3d443d9f8d2cec4a0e7cbe1866
Michael <michael@adl-crm.uk>
2018-08-23 12:44:30 +0100

    Completed KnpMarkdownBundle & its Services

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,12 +8,10 @@
 
 namespace App\Controller;
 
+use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -22,7 +20,8 @@ class ArticleController extends AbstractController
      * @Route("/", name="app_homepage")
      */
 
-    public function homepage() {
+    public function homepage()
+    {
 
         return $this->render('article/homepage.html.twig');
 
@@ -32,20 +31,43 @@ class ArticleController extends AbstractController
      * @Route("/news/{slug}", name="article_show")
      */
 
-    public function show($slug)
-        {
-            $comments = [
-                'I ate a normal rock once. It did NOT taste like bacon!',
-                'Woohoo! I\'m going on an all-asteroid diet!',
-                'I like bacon too! Buy some from my site! bakinsomebacon.com',
-            ];
+    public function show($slug, MarkdownInterface $markdown)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
 
-            return $this->render('article/show.html.twig', [
-                'title' => ucwords(str_replace('-', ' ', $slug)),
-                'slug' => $slug,
-                'comments' => $comments,
-            ]);
-        }
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim.
+
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF;
+
+        $articleContent = $markdown->transform($articleContent);
+
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'articleContent' => $articleContent,
+            'slug' => $slug,
+            'comments' => $comments,
+        ]);
+    }
 
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
@@ -56,7 +78,7 @@ class ArticleController extends AbstractController
 
         $logger->info('Article is being hearted!');
 
-        return $this->json(['hearts' => rand(5,100)]);
+        return $this->json(['hearts' => rand(5, 100)]);
 
     }
 
