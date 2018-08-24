a1fb1f016984f18f1e03e81c558b0f4bf118130e
Michael <michael@adl-crm.uk>
2018-08-24 12:40:44 +0100

    Completed Using Non-Standard Services: Logger Channels

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -10,6 +10,7 @@ namespace App\Service;
 
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
@@ -18,15 +19,24 @@ class MarkdownHelper
     private $cache;
 
     private $markdown;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     public function parse(string $source): string
     {
+
+        if(stripos($source, 'bacon') !== false) {
+            $this->logger->info("They are talking about bacon again");
+        }
 
         $item = $this->cache->getItem('markfown_'.md5($source));
 
