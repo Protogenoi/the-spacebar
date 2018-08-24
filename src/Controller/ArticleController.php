c2c4a80bcb8b325df875779624840edaaf2e1ead
Michael <michael@adl-crm.uk>
2018-08-24 10:49:32 +0100

    Completed debug:container & Cache Config. Had to install backwards compatability for APCu for PHP7.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -68,9 +68,6 @@ EOF;
 
         $articleContent = $item->get();
 
-        dump($markdown);die;
-
-
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'articleContent' => $articleContent,
