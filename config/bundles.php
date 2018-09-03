8731ddae4f0fb51626c6a7e0436bb959edb2fb93
Michael <michael@adl-crm.uk>
2018-09-03 17:33:58 +0100

    Doctrine & the Database: Completed chapter 1: Installing Doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Nexy\SlackBundle\NexySlackBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
