31fe12bf082e41799140310ec9f370b43176db2d
Michael <michael@adl-crm.uk>
2018-09-04 14:53:57 +0100

    Doctrine & the Database: Completed chapter 7: ago Filter with KnpTimeBundle!

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
