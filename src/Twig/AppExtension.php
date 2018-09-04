7c671b929aef36edd56964319134116ea9a6fbfc
Michael <michael@adl-crm.uk>
2018-09-04 14:55:03 +0100

    Doctrine & the Database: Completed chapter 7: ago Filter with KnpTimeBundle!

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    /**
+     * @var MarkdownHelper
+     */
+    private $helper;
+
+    public function __construct(MarkdownHelper $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'],['is_safe' => ['html']]),
+        ];
+    }
+
+    public function processMarkdown($value)
+    {
+        return $this->helper->parse($value);
+    }
+}
