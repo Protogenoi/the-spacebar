492575b43a2e7b227990458b658c6f2f78c5d559
Michael <michael@adl-crm.uk>
2018-09-05 12:45:39 +0100

    Doctrine & the Database: Completed chapter 14: Fixtures: Seeding Dummy Data!

---
+++ src/DataFixtures/BaseFixtures.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 05/09/2018
+ * Time: 12:31
+ */
+
+namespace App\DataFixtures;
+
+
+use function Clue\StreamFilter\fun;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+abstract class BaseFixtures extends Fixture
+{
+
+    /**
+     * @var ObjectManager
+     */
+
+    private $manager;
+
+    abstract protected function loadData(ObjectManager $em);
+
+    public function load(ObjectManager $manager)
+    {
+
+       $this->manager = $manager;
+
+       $this->loadData($manager);
+
+    }
+
+    protected function createMany(string $className, int $count, callable $factory)
+    {
+
+        for ($i =0; $i < $count; $i++) {
+            $entity = new $className();
+            $factory($entity, $i);
+
+            $this->manager->persist($entity);
+            //store for useage later as App/Entity/ClassName_#COUNT#
+        $this->addReference($className . '_' . $i, $entity);
+    }
+
+    }
+
+}
---
+++ src/Migrations/Version20180905104515.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180905104515 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(100) NOT NULL, content LONGTEXT DEFAULT NULL, published_at DATETIME DEFAULT NULL, author VARCHAR(255) NOT NULL, heart_count INT NOT NULL, image_filename VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_23A0E66989D9B62 (slug), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE article');
+    }
+}
