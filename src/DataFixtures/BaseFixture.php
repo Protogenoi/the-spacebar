9b6402a8a31d304e1ddbabdd277b45603efd07fc
Michael <michael@adl-crm.uk>
2018-09-06 16:44:08 +0100

    Mastering Doctrine Relations! Completed chapter 7: Awesome Random Fixtures

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -26,6 +26,8 @@ abstract class BaseFixture extends Fixture
 
     protected $faker;
 
+    private $referencesIndex = [];
+
     abstract protected function loadData(ObjectManager $em);
 
     public function load(ObjectManager $manager)
@@ -52,4 +54,19 @@ abstract class BaseFixture extends Fixture
 
     }
 
+    protected function getRandomReference(string $className) {
+        if (!isset($this->referencesIndex[$className])) {
+            $this->referencesIndex[$className] = [];
+            foreach ($this->referenceRepository->getReferences() as $key => $ref) {
+                if (strpos($key, $className.'_') === 0) {
+                    $this->referencesIndex[$className][] = $key;
+                }
+            }
+        }
+        if (empty($this->referencesIndex[$className])) {
+            throw new \Exception(sprintf('Cannot find any references for class "%s"', $className));
+        }
+        $randomReferenceKey = $this->faker->randomElement($this->referencesIndex[$className]);
+        return $this->getReference($randomReferenceKey);
+    }
 }
--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CommentFixture extends BaseFixture
+class CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
@@ -19,10 +20,19 @@ class CommentFixture extends BaseFixture
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 month','-1 seconds'));
 
-            $comment->setArticle($this->getReference(Article::class.'_0'));
+            $comment->setArticle($this->getRandomReference(Article::class));
 
         });
 
         $manager->flush();
+
+
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ArticleFixture::class,
+        ];
     }
 }
