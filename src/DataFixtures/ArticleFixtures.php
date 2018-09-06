47802df380616d8ead98b539dfc77f66b2993d66
Michael <michael@adl-crm.uk>
2018-09-06 15:02:56 +0100

    Mastering Doctrine Relations! Completed chapter 5: Owning Vs Inverse Relations

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -62,7 +62,7 @@ EOF
             $comment1 = new Comment();
             $comment1->setAuthorName('Mike Ferengi ');
             $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');
-            $comment1->setArticle($article);
+           // $comment1->setArticle($article);
             $manager->persist($comment1);
 
             $comment2 = new Comment();
