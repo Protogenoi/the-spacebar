404fdd20fbfda3dcd176c822de101e0bfbbb328b
Michael <michael@adl-crm.uk>
2018-09-06 12:24:20 +0100

    Mastering Doctrine Relations! Completed chapter 2: Saving Relations

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixtures
@@ -27,7 +28,7 @@ class ArticleFixtures extends BaseFixtures
     {
 
 
-        $this->createMany(Article::class, 10, function (Article $article, $count) {
+        $this->createMany(Article::class, 10, function (Article $article, $count) use($manager) {
             $article->setTitle($this->faker->randomElement(self::$articleTitles))
                 ->setContent(<<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
@@ -57,6 +58,18 @@ EOF
             $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
                 ->setHeartCount($this->faker->numberBetween(5,100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages));
+
+            $comment1 = new Comment();
+            $comment1->setAuthorName('Mike Ferengi ');
+            $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');
+            $comment1->setArticle($article);
+            $manager->persist($comment1);
+
+            $comment2 = new Comment();
+            $comment2->setAuthorName('Mike Ferengi ');
+            $comment2->setContent('Woohoo! I\'m going on an all-asteroid diet!');
+            $comment2->setArticle($article);
+            $manager->persist($comment2);
 
         });
 
