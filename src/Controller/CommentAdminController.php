9abd3d3efff98a4920d6cfc16de3107c51b00f39
Michael <michael@adl-crm.uk>
2018-09-07 11:18:29 +0100

    Mastering Doctrine Relations! Completed chapter 12: The Twig Extensions Library

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,13 @@ class CommentAdminController extends AbstractController
     /**
      * @Route("/admin/comment/admin", name="comment_admin")
      */
-    public function index()
+    public function index(CommentRepository $repository)
     {
+
+        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
