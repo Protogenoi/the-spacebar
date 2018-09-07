a6ce42f1772ab0a031f1a6f6fd40ad8d5f94d34e
Michael <michael@adl-crm.uk>
2018-09-07 14:26:53 +0100

    Mastering Doctrine Relations! Completed chapter 16: The 4 (2?) Possible Relation Types

---
+++ src/DataFixtures/TagFixture.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Tag;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+class TagFixture extends BaseFixture
+{
+    protected function loadData(ObjectManager $manager)
+    {
+
+        $this->createMany(Tag::class, 10, function (Tag $tag) {
+            $tag->setName($this->faker->realText(20));
+
+        });
+
+        $manager->flush();
+    }
+}
