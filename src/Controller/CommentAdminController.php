b8021b59f3020a8dd3e262120f3946f350724c6c
Michael <michael@adl-crm.uk>
2018-09-07 11:43:30 +0100

    Mastering Doctrine Relations! Completed chapter 13: Request Object & Query OR Logic

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentAdminController extends AbstractController
@@ -11,10 +12,12 @@ class CommentAdminController extends AbstractController
     /**
      * @Route("/admin/comment/admin", name="comment_admin")
      */
-    public function index(CommentRepository $repository)
+    public function index(CommentRepository $repository, Request $request)
     {
 
-        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+        $q = $request->query->get('q');
+
+        $comments = $repository->findAllWithSearch($q);
 
         return $this->render('comment_admin/index.html.twig', [
             'comments' => $comments,
