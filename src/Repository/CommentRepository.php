55c0f239799c145a8e1aa42e6f6b4588667a21ab
Michael <michael@adl-crm.uk>
2018-09-07 13:20:58 +0100

    Mastering Doctrine Relations! Completed chapter 15: Pagination

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Comment;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
@@ -21,10 +22,9 @@ class CommentRepository extends ServiceEntityRepository
 
     /**
      * @param null|string $term
-     * @return Comment[]
      */
 
-    public function findAllWithSearch(?string $term)
+    public function getWithSearchQueryBuilder(?string $term): QueryBuilder
     {
 
     $qb = $this->createQueryBuilder('c')
@@ -38,9 +38,7 @@ class CommentRepository extends ServiceEntityRepository
     }
 
     return $qb
-        ->orderBy('c.createdAt', 'DESC')
-        ->getQuery()
-        ->getResult();
+        ->orderBy('c.createdAt', 'DESC');
 
     }
 
