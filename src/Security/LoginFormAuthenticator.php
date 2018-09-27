c90eafb1022a40aa797f38a967ee8bffd20c5d32
Michael <michael@adl-crm.uk>
2018-09-27 12:23:38 +0100

    Completed chapter 14: Target Path: Redirecting an Anonymous User

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -16,9 +16,12 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
+
+    use TargetPathTrait;
 
     private $userRepository;
 
@@ -102,6 +105,13 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         TokenInterface $token,
         $providerKey
     ) {
+
+        if ($targetPath = $this->getTargetPath($request->getSession(),
+            $providerKey)
+        ) {
+            return new RedirectResponse($targetPath);
+        }
+
         return new RedirectResponse($this->router->generate('app_homepage'));
     }
 
