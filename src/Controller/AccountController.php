19e9994829e1dddd58280d746eb42f90e6adcc0f
Michael <michael@adl-crm.uk>
2018-10-03 12:02:21 +0100

    Completed chapter 18: Fetch the User Object

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,20 +2,24 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @IsGranted("ROLE_USER")
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+
+        $logger->debug('Checking account page for ' . $this->getUser()
+                ->getEmail());
+
         return $this->render('account/index.html.twig');
     }
 }
---
+++ src/Controller/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 03/10/2018
+ * Time: 11:43
+ */
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+abstract class BaseController extends AbstractController
+{
+    protected function getUser(): User
+    {
+
+        return parent::getUser();
+    }
+
+}
