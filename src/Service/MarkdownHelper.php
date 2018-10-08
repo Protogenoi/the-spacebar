3754a790219b69f7140f0e1f8cfc56c122be5d68
Michael <michael@adl-crm.uk>
2018-10-08 10:48:00 +0100

    Completed chapter 20: Fetching the User In a Service

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -12,6 +12,7 @@ namespace App\Service;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Security\Core\Security;
 
 class MarkdownHelper
 {
@@ -27,20 +28,33 @@ class MarkdownHelper
      * @var bool
      */
     private $isDebug;
+    /**
+     * @var Security
+     */
+    private $security;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
+    public function __construct(
+        AdapterInterface $cache,
+        MarkdownInterface $markdown,
+        LoggerInterface $markdownLogger,
+        bool $isDebug,
+        Security $security
+    )
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
         $this->isDebug = $isDebug;
+        $this->security = $security;
     }
 
     public function parse(string $source): string
     {
 
         if(stripos($source, 'bacon') !== false) {
-            $this->logger->info("They are talking about bacon again");
+            $this->logger->info("They are talking about bacon again", [
+                'user' => $this->security->getUser(),
+            ]);
         }
 
         if($this->isDebug) {
