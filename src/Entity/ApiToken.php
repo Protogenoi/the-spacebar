af391e361c5ee3cf90541e7393eafddb978d4c12
Michael <michael@adl-crm.uk>
2018-10-22 11:24:31 +0100

    Completed chapter 28: API Token Authenticator Part 2!

--- src/Entity/ApiToken.php
+++ src/Entity/ApiToken.php
@@ -59,4 +59,9 @@ class ApiToken
         return $this->user;
     }
 
+    public function isExpired(): bool
+    {
+        return $this->getExpiresAt() <= new \DateTime();
+    }
+
 }
--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -3,9 +3,11 @@
 namespace App\Security;
 
 use App\Repository\ApiTokenRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
@@ -40,8 +42,14 @@ class ApiTokenAuthenticator extends AbstractGuardAuthenticator
         $token = $this->apiTokenRepository->findOneBy([
             'token' => $credentials,
         ]);
+
         if (!$token) {
-            return;
+            throw new CustomUserMessageAuthenticationException('Invalid API Token');
+        }
+
+        if ($token->isExpired()) {
+            throw new CustomUserMessageAuthenticationException('Token Expired');
+
         }
 
         return $token->getUser();
@@ -49,14 +57,16 @@ class ApiTokenAuthenticator extends AbstractGuardAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        dd('checking credentials');
+        return true;
     }
 
     public function onAuthenticationFailure(
         Request $request,
         AuthenticationException $exception
     ) {
-        // todo
+        return new JsonResponse([
+            'message' => $exception->getMessageKey(),
+        ], 401);
     }
 
     public function onAuthenticationSuccess(
@@ -64,18 +74,18 @@ class ApiTokenAuthenticator extends AbstractGuardAuthenticator
         TokenInterface $token,
         $providerKey
     ) {
-        // todo
+        // allow the request to continue
     }
 
     public function start(
         Request $request,
         AuthenticationException $authException = null
     ) {
-        // todo
+        throw new \Exception('Not used: entry_point from other authenticator is used');
     }
 
     public function supportsRememberMe()
     {
-        // todo
+        return false;
     }
 }
