43623f3556b501e89987d15e4acf3568bdf35a0e
Michael <michael@adl-crm.uk>
2018-10-31 13:07:58 +0000

    Completed chapter 33: Adding a Custom Voter

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -37,13 +37,10 @@ class ArticleAdminController extends AbstractController
 
     /**
      * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
      */
     public function edit(Article $article)
     {
-
-        if (!$this->isGranted('MANAGE', $article)) {
-            throw $this->createAccessDeniedException('No access!');
-        }
 
         dd($article);
     }
