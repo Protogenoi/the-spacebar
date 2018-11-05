2fd77d8f8793fa23e847a9380114d72e8a0a0f69
Michael <michael@adl-crm.uk>
2018-11-05 15:17:34 +0000

    Symfony 4 Forms: Build, Render & Conquer!: Completed chapter 5: Field Types & Options

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'help' => "Choose something catchty!",
+            ])
             ->add('content');
     }
 
