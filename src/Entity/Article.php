521e9f15c8c01a52a53194acd11e05b446d8e70a
Michael <michael@adl-crm.uk>
2018-11-09 14:40:13 +0000

    Symfony 4 Forms: Build, Render & Conquer!: Chapter 10: Validation Constraints with @Assert

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -9,6 +9,8 @@ use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -26,6 +28,7 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Get creative and think of a title!")
      */
     private $title;
 
@@ -251,6 +254,21 @@ class Article
         $this->author = $author;
 
         return $this;
+    }
+
+    /**
+     * @Assert\Callback
+     */
+    public function validate(ExecutionContextInterface $context, $payload)
+    {
+
+        if (stripos($this->getTitle(), 'the borg') !== false) {
+
+            $context->buildViolation('Um.. the borg makes us nervous')
+                ->atPath('title')
+                ->addViolation();
+        }
+
     }
 
 }
