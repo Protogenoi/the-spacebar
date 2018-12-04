bc55c22e14e0a606adcb09cd83320e3a1a14d3d2
Michael <michael@adl-crm.uk>
2018-12-04 11:24:43 +0000

    Symfony 4 Forms: Build, Render & Conquer!: Chapter 24: Data Transformer

---
+++ src/Form/EmailToUserTransformer.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class EmailToUserTransformer implements DataTransformerInterface
+{
+
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+
+        $this->userRepository = $userRepository;
+    }
+
+
+    public function transform($value)
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        if (!$value instanceof User) {
+            throw new \LogicException('The UserSelectTextType can only be used with User Objects');
+        }
+
+        return $value->getEmail();
+    }
+
+    public function reverseTransform($value)
+    {
+        $user = $this->userRepository->findOneBy(['email' => $value]);
+
+        if (!$user) {
+            throw new TransformationFailedException(sprintf(
+                'No user found with email "%s"',
+                $value
+            ));
+        }
+
+        return $user;
+
+    }
+
+}
--- src/Form/UserSelectTextType.php
+++ src/Form/UserSelectTextType.php
@@ -9,11 +9,27 @@
 namespace App\Form;
 
 
+use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class UserSelectTextType extends AbstractType
 {
+
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new EmailToUserTransformer($this->userRepository));
+    }
+
+
     public function getParent()
     {
         return TextType::class;
