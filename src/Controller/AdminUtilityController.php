769e9cc86979d1d853ef6327880a135a505f47ac
Michael <michael@adl-crm.uk>
2018-12-13 11:07:36 +0000

    Symfony 4 Forms: Build, Render & Conquer!: Chapter 28: Autocomplete Endpoint & Serialization Group

---
+++ src/Controller/AdminUtilityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminUtilityController extends AbstractController
+{
+
+    /**
+     * @Route("/admin/utility/users", methods="GET")
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
+     */
+
+    public function getUsersApi(UserRepository $userRepository)
+    {
+
+        $users = $userRepository->findAllEmailAlphabetical();
+
+        return $this->json([
+            'users' => $users,
+        ], 200, [], ['groups' => ['main']]);
+
+    }
+
+}
